76f0fb882ac722626fb48315d7cc291ed3b4f1e5
Chaimahadjkacem <92598511+Chaimahadjkacem@users.noreply.github.com>
2023-02-04 19:59:39 +0100

    Add files via upload

---
+++ HadjKacemChaima/AM.UI.Console/Program.cs
@@ -0,0 +1,42 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using AM.ApplicationCore.Domain;
+
+Plane p = new Plane();
+
+p.PlaneId = 1;
+p.Capacity = 100;
+p.ManufactureDate = DateTime.Now;
+p.PlaneType = PlaneType.Boing;
+
+Console.WriteLine(p);
+
+//Plane p2 = new Plane(200 , new DateTime (2023,01,03) , PlaneType.Airbus);
+//Console.WriteLine(p2);
+
+Plane p3 = new Plane()
+{
+
+    Capacity = 100,
+    ManufactureDate = DateTime.Now,
+    PlaneType = PlaneType.Boing
+
+};
+Console.WriteLine(p3); // Rq : L'ordre dans le constructeur n'est pas important ! 
+
+Passenger pas = new Passenger();
+pas.PassengerType();
+
+Passenger pas1 = new Staff();
+pas1.PassengerType();
+
+Passenger pas2 = new Traveller();
+pas2.PassengerType();
+
+
+
+
+
+
+

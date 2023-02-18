28f78425a3d6fb0282e8c56373466e2054d08fb1
Chaimahadjkacem <92598511+Chaimahadjkacem@users.noreply.github.com>
2023-02-18 16:54:11 +0100

    Add files via upload

---
+++ HadjKacemChaima/HadjKacemChaima/AM.ApplicationCore/Domain/Passenger.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public class Passenger
+    {
+        public DateTime BirthDate { get; set; }
+        public int PassportNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int TelNumber { get; set; }
+
+        public IList<Flight> Flights { get; set; }
+
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + " " + TelNumber + " " + BirthDate + " " + EmailAddress + " " + PassportNumber;
+        }
+
+        //public bool CheckProfile( string prenom , string nom)
+        //{
+           
+        //    return nom == LastName && prenom == FirstName;
+        //}
+        //public bool CheckProfile(string prenom, string nom , string email)
+        //{
+
+        //    return nom == LastName && prenom == FirstName && email == EmailAddress;
+        //}
+        public bool CheckProfile(string prenom, string nom, string email = null)
+        {
+            if (email == null)
+            {
+                return nom == LastName && prenom == FirstName;
+            }
+            return nom == LastName && prenom == FirstName && email == EmailAddress;
+
+        }
+
+        public virtual void PassengerType()
+        {
+            Console.WriteLine("I am a passenger");
+        }
+
+
+    }
+}
---
+++ HadjKacemChaima/HadjKacemChaima/AM.ApplicationCore/Interfaces/IServiceFlight.cs
@@ -0,0 +1,33 @@
+﻿using AM.ApplicationCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Interfaces
+{
+    public interface IServiceFlight
+    {
+        public List<DateTime> GetFlightDates(string destination);
+        //   public List<Flight> GetFlights(string filterType, string filterValue);
+        public List<Flight> GetFlights(string filterValue, Func<Flight, string, Boolean> condition);
+
+         public void ShowFlightDetails(Plane plane);
+
+        public int ProgrammedFlightNumber(DateTime startDate);
+
+        public double DurationAverage(string destination);
+
+        public List<Flight> OrderedDurationFlights();
+
+        public List<Traveller> SeniorTravellers(Flight flight);
+
+        public void DestinationGroupedFlights();
+
+        // public void ShowFlightDetails(Action<Plane> FlightDetailsDel);
+
+        // public double DurationAverage(Func<string, float> DurationAverageDel);
+
+    }
+}
